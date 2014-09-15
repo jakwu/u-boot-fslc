71a75d02fbdb5a10e1140a74aa4f1e3744d9dbc8
jakwu <jak.wu@web.de>
2014-09-16 00:16:21 +0200

    Some changes and fixes in mx6embest_common.h
    - Changed indentation.
    - Changed loadimage to load the kernel directly from mmc device.
    - Fixed CONFIG_MMCROOT (changed to /dev/mmcblk0p1).
    - Changed kernel image tu uImage.
    - Changed I2C speed to 25kHz.

--- include/configs/mx6embest_common.h
+++ include/configs/mx6embest_common.h
@@ -21,11 +21,9 @@
 
 #define CONFIG_SYS_GENERIC_BOARD
 
-#define CONFIG_MXC_UART_BASE		UART2_BASE
-#define CONFIG_CONSOLE_DEV		"ttymxc1"
-#define CONFIG_MMCROOT			"/dev/mmcblk1p2"
-
-#define PHYS_SDRAM_SIZE		(1u * 1024 * 1024 * 1024)
+#define CONFIG_MXC_UART_BASE            UART2_BASE
+#define CONFIG_CONSOLE_DEV              "ttymxc1"
+#define CONFIG_MMCROOT                  "/dev/mmcblk0p1"
 
 #define CONFIG_MX6
 
@@ -38,7 +36,7 @@
 #define CONFIG_REVISION_TAG
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		(10 * SZ_1M)
+#define CONFIG_SYS_MALLOC_LEN           (10 * SZ_1M)
 
 #define CONFIG_BOARD_EARLY_INIT_F
 #define CONFIG_BOARD_LATE_INIT
@@ -48,14 +46,15 @@
 
 #define CONFIG_CMD_FUSE
 #ifdef CONFIG_CMD_FUSE
-#define CONFIG_MXC_OCOTP
+	#define CONFIG_MXC_OCOTP
 #endif
 
 /* I2C Configs */
 #define CONFIG_CMD_I2C
 #define CONFIG_SYS_I2C
 #define CONFIG_SYS_I2C_MXC
-#define CONFIG_SYS_I2C_SPEED		100000
+#define CONFIG_SYS_I2C_SPEED            100000
+#define CONFIG_SYS_I2C_SLAVE            0x1f
 
 /* USB Configs */
 #define CONFIG_CMD_USB
@@ -65,14 +64,14 @@
 #define CONFIG_USB_HOST_ETHER
 #define CONFIG_USB_ETHER_ASIX
 #define CONFIG_USB_MAX_CONTROLLER_COUNT 2
-#define CONFIG_EHCI_HCD_INIT_AFTER_RESET	/* For OTG port */
-#define CONFIG_MXC_USB_PORTSC	(PORT_PTS_UTMI | PORT_PTS_PTW)
-#define CONFIG_MXC_USB_FLAGS	0
+#define CONFIG_EHCI_HCD_INIT_AFTER_RESET /* For OTG port */
+#define CONFIG_MXC_USB_PORTSC           (PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_MXC_USB_FLAGS            0
 
 /* MMC Configs */
 #define CONFIG_FSL_ESDHC
 #define CONFIG_FSL_USDHC
-#define CONFIG_SYS_FSL_ESDHC_ADDR      0
+#define CONFIG_SYS_FSL_ESDHC_ADDR       0
 
 #define CONFIG_MMC
 #define CONFIG_CMD_MMC
@@ -88,28 +87,28 @@
 #define CONFIG_CMD_NET
 #define CONFIG_FEC_MXC
 #define CONFIG_MII
-#define IMX_FEC_BASE			ENET_BASE_ADDR
-#define CONFIG_FEC_XCV_TYPE		RGMII
-#define CONFIG_ETHPRIME			"FEC"
-#define CONFIG_FEC_MXC_PHYADDR		4
+#define IMX_FEC_BASE                    ENET_BASE_ADDR
+#define CONFIG_FEC_XCV_TYPE             RGMII
+#define CONFIG_ETHPRIME                 "FEC"
+#define CONFIG_FEC_MXC_PHYADDR          4
 
 #define CONFIG_PHYLIB
 #define CONFIG_PHY_ATHEROS
 
 #ifdef CONFIG_CMD_SF
-#define CONFIG_SPI_FLASH
-#define CONFIG_SPI_FLASH_SST
-#define CONFIG_MXC_SPI
-#define CONFIG_SF_DEFAULT_BUS		0
-#define CONFIG_SF_DEFAULT_CS		(0 | (IMX_GPIO_NR(2, 30) << 8))
-#define CONFIG_SF_DEFAULT_SPEED		20000000
-#define CONFIG_SF_DEFAULT_MODE		SPI_MODE_0
+	#define CONFIG_SPI_FLASH
+	#define CONFIG_SPI_FLASH_SST
+	#define CONFIG_MXC_SPI
+	#define CONFIG_SF_DEFAULT_BUS       0
+	#define CONFIG_SF_DEFAULT_CS        (0 | (IMX_GPIO_NR(2, 30) << 8))
+	#define CONFIG_SF_DEFAULT_SPEED     25000000
+	#define CONFIG_SF_DEFAULT_MODE      SPI_MODE_0
 #endif
 
 /* allow to overwrite serial and ethaddr */
 #define CONFIG_ENV_OVERWRITE
-#define CONFIG_CONS_INDEX              1
-#define CONFIG_BAUDRATE                        115200
+#define CONFIG_CONS_INDEX               1
+#define CONFIG_BAUDRATE                 115200
 
 /* Command definition */
 #include <config_cmd_default.h>
@@ -119,54 +118,56 @@
 #define CONFIG_CMD_SETEXPR
 #undef CONFIG_CMD_IMLS
 
-#define CONFIG_BOOTDELAY               1
+#define CONFIG_LOADADDR                 0x12000000
+#define CONFIG_RD_LOADADDR              0x12C00000
+#define CONFIG_FTD_LOADADDR             0x18000000
+#define CONFIG_SYS_TEXT_BASE            0x17800000
 
-#define CONFIG_LOADADDR                        0x12000000
-#define CONFIG_SYS_TEXT_BASE           0x17800000
+#define CONFIG_BOOTDELAY            3
 
 #ifdef CONFIG_SUPPORT_EMMC_BOOT
-#define EMMC_ENV \
-	"emmcdev=2\0" \
-	"update_emmc_firmware=" \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"if ${get_cmd} ${update_sd_firmware_filename}; then " \
-			"if mmc dev ${emmcdev}; then "	\
-				"setexpr fw_sz ${filesize} / 0x200; " \
-				"setexpr fw_sz ${fw_sz} + 1; "	\
-				"mmc write ${loadaddr} 0x2 ${fw_sz}; " \
-			"fi; "	\
-		"fi\0"
+	#define EMMC_ENV \
+		"emmcdev=2\0" \
+		"update_emmc_firmware=" \
+			"if test ${ip_dyn} = yes; then " \
+				"setenv get_cmd dhcp; " \
+			"else " \
+				"setenv get_cmd tftp; " \
+			"fi; " \
+			"if ${get_cmd} ${update_sd_firmware_filename}; then " \
+				"if mmc dev ${emmcdev}; then "	\
+					"setexpr fw_sz ${filesize} / 0x200; " \
+					"setexpr fw_sz ${fw_sz} + 1; "	\
+					"mmc write ${loadaddr} 0x2 ${fw_sz}; " \
+				"fi; "	\
+			"fi\0"
 #else
-#define EMMC_ENV ""
+	#define EMMC_ENV ""
 #endif
 
 #ifdef CONFIG_CMD_SF
-#define SF_ENV \
-	"update_spi_firmware=" \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"if ${get_cmd} ${update_spi_firmware_filename}; then " \
-			"if sf probe; then "	\
-				"sf erase 0 0xc0000; " \
-				"sf write ${loadaddr} 0x400 ${filesize}; " \
-			"fi; "	\
-		"fi\0"
+	#define SF_ENV \
+		"update_spi_firmware=" \
+			"if test ${ip_dyn} = yes; then " \
+				"setenv get_cmd dhcp; " \
+			"else " \
+				"setenv get_cmd tftp; " \
+			"fi; " \
+			"if ${get_cmd} ${update_spi_firmware_filename}; then " \
+				"if sf probe; then "	\
+					"sf erase 0 0xc0000; " \
+					"sf write ${loadaddr} 0x400 ${filesize}; " \
+				"fi; "	\
+			"fi\0"
 #else
-#define SF_ENV ""
+	#define SF_ENV ""
 #endif
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"script=boot.scr\0" \
-	"image=zImage\0" \
+	"image=uImage\0" \
 	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	"fdt_addr=0x18000000\0" \
+	"fdt_addr=" __stringify(CONFIG_FTD_LOADADDR) "\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
 	"console=" CONFIG_CONSOLE_DEV "\0" \
@@ -190,30 +191,31 @@
 		"fi\0" \
 	EMMC_ENV	  \
 	SF_ENV	  \
-	"mmcargs=setenv bootargs console=${console},${baudrate} " \
+	"mmcargs=setenv bootargs console=${console},${baudrate} ${dspargs} " \
 		"root=${mmcroot}\0" \
 	"loadbootscript=" \
-		"fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
+		"ext2load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"loadimage=mmc read ${loadaddr} 0x800 0x3000\0" \
+	"loadfdt=ext2load mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
 	"mmcboot=echo Booting from mmc ...; " \
 		"run mmcargs; " \
+		"echo command line \"${bootargs}\"; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if run loadfdt; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
+				"bootm ${loadaddr} - ${fdt_addr}; " \
 			"else " \
 				"if test ${boot_fdt} = try; then " \
-					"bootz; " \
+					"bootm; " \
 				"else " \
 					"echo WARN: Cannot load the DT; " \
 				"fi; " \
 			"fi; " \
 		"else " \
-			"bootz; " \
+			"bootm; " \
 		"fi;\0" \
-	"netargs=setenv bootargs console=${console},${baudrate} " \
+	"netargs=setenv bootargs console=${console},${baudrate} $(dspargs) " \
 		"root=/dev/nfs " \
 		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
 	"netboot=echo Booting from net ...; " \
@@ -226,23 +228,23 @@
 		"${get_cmd} ${image}; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
+				"bootm ${loadaddr} - ${fdt_addr}; " \
 			"else " \
 				"if test ${boot_fdt} = try; then " \
-					"bootz; " \
+					"bootm; " \
 				"else " \
 					"echo WARN: Cannot load the DT; " \
 				"fi; " \
 			"fi; " \
 		"else " \
 			"bootz; " \
-		"fi;\0"
+		"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
-	"mmc dev ${mmcdev};" \
+	"mmc dev ${mmcdev}; " \
 	"if mmc rescan; then " \
 		"if run loadbootscript; then " \
-		"run bootscript; " \
+			"run bootscript; " \
 		"else " \
 			"if run loadimage; then " \
 				"run mmcboot; " \
@@ -256,31 +258,32 @@
 /* Miscellaneous configurable options */
 #define CONFIG_SYS_LONGHELP
 #define CONFIG_SYS_HUSH_PARSER
-#define CONFIG_SYS_PROMPT_HUSH_PS2     "> "
+#define CONFIG_SYS_PROMPT_HUSH_PS2      "> "
 #define CONFIG_AUTO_COMPLETE
-#define CONFIG_SYS_CBSIZE              256
+#define CONFIG_SYS_CBSIZE               256
 
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE (CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
-#define CONFIG_SYS_MAXARGS             16
+#define CONFIG_SYS_MAXARGS              16
 #define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
 
-#define CONFIG_SYS_MEMTEST_START       0x10000000
-#define CONFIG_SYS_MEMTEST_END         0x10010000
-#define CONFIG_SYS_MEMTEST_SCRATCH     0x10800000
+#define CONFIG_SYS_MEMTEST_START        0x10000000
+#define CONFIG_SYS_MEMTEST_END          0x10010000
+#define CONFIG_SYS_MEMTEST_SCRATCH      0x10800000
 
-#define CONFIG_SYS_LOAD_ADDR           CONFIG_LOADADDR
+#define CONFIG_SYS_LOAD_ADDR            CONFIG_LOADADDR
 
 #define CONFIG_CMDLINE_EDITING
-#define CONFIG_STACKSIZE               (128 * 1024)
+#define CONFIG_STACKSIZE                (128 * 1024)
 
 /* Physical Memory Map */
-#define CONFIG_NR_DRAM_BANKS           1
-#define PHYS_SDRAM                     MMDC0_ARB_BASE_ADDR
+#define CONFIG_NR_DRAM_BANKS            1
+#define PHYS_SDRAM                      MMDC0_ARB_BASE_ADDR
+#define PHYS_SDRAM_SIZE                 SZ_1G
 
-#define CONFIG_SYS_SDRAM_BASE          PHYS_SDRAM
-#define CONFIG_SYS_INIT_RAM_ADDR       IRAM_BASE_ADDR
-#define CONFIG_SYS_INIT_RAM_SIZE       IRAM_SIZE
+#define CONFIG_SYS_SDRAM_BASE           PHYS_SDRAM
+#define CONFIG_SYS_INIT_RAM_ADDR        IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE        IRAM_SIZE
 
 #define CONFIG_SYS_INIT_SP_OFFSET \
 	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
@@ -290,26 +293,28 @@
 /* FLASH and environment organization */
 #define CONFIG_SYS_NO_FLASH
 
-#define CONFIG_ENV_SIZE			(8 * 1024)
+#define CONFIG_ENV_SIZE                 (8 * 1024)
 
 #if defined(CONFIG_ENV_IS_IN_MMC)
-/* RiOTboard */
-#define CONFIG_ENV_OFFSET		(6 * 64 * 1024)
-#define CONFIG_SUPPORT_EMMC_BOOT /* eMMC specific */
+	/* RiOTboard */
+	#define CONFIG_ENV_OFFSET           (6 * 64 * 1024)
+	#define CONFIG_SUPPORT_EMMC_BOOT /* eMMC specific */
 #elif defined(CONFIG_ENV_IS_IN_SPI_FLASH)
-/* MarSBoard */
-#define CONFIG_ENV_OFFSET		(768 * 1024)
-#define CONFIG_ENV_SECT_SIZE		(8 * 1024)
-#define CONFIG_ENV_SPI_BUS		CONFIG_SF_DEFAULT_BUS
-#define CONFIG_ENV_SPI_CS		CONFIG_SF_DEFAULT_CS
-#define CONFIG_ENV_SPI_MODE		CONFIG_SF_DEFAULT_MODE
-#define CONFIG_ENV_SPI_MAX_HZ		CONFIG_SF_DEFAULT_SPEED
+	/* MarSBoard */
+	#define CONFIG_ENV_OFFSET           (768 * 1024)
+	#define CONFIG_ENV_SECT_SIZE        (8 * 1024)
+	#define CONFIG_ENV_SPI_BUS          CONFIG_SF_DEFAULT_BUS
+	#define CONFIG_ENV_SPI_CS           CONFIG_SF_DEFAULT_CS
+	#define CONFIG_ENV_SPI_MODE         CONFIG_SF_DEFAULT_MODE
+	#define CONFIG_ENV_SPI_MAX_HZ       CONFIG_SF_DEFAULT_SPEED
+#else
+	#define CONFIG_ENV_IS_NOWHERE       1
 #endif
 
 #define CONFIG_OF_LIBFDT
 
 #ifndef CONFIG_SYS_DCACHE_OFF
-#define CONFIG_CMD_CACHE
+	#define CONFIG_CMD_CACHE
 #endif
 
 /* Framebuffer */
